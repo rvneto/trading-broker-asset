543891621672a100416d47bb23d8ed243c9d9036
roberto <rvneto.rs@gmail.com>
2026-03-28 11:55:13 -0300

    initial version

---
+++ src/main/java/com/rvneto/broker/asset/BrokerAssetApplication.java
@@ -0,0 +1,13 @@
+package com.rvneto.broker.asset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerAssetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokerAssetApplication.class, args);
+	}
+
+}
